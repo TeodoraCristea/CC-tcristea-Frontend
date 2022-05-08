d80e4fcba562230683f6f45ad3a26c39466dfa04
TeodoraCristea <72539275+TeodoraCristea@users.noreply.github.com>
2022-05-08 20:01:37 +0300

    Update SMSesSubmit.jsx

--- src/javascript/components/SMSesSubmit.jsx
+++ src/javascript/components/SMSesSubmit.jsx
@@ -29,7 +29,7 @@ function SMSesSubmit() {
     return (
         <div id="SMSesSubmit">
             <div className='text-2xl font-bold mb-4'>Submit your SMS</div>
-            <div className='text-xs mb-4'><p style={{ color: 'red' }}>The Admin will receive an audit log for this.</p></div>
+            <div className='text-xs mb-4'><p style={{ color: 'red' }}>The Admin will receive an audit log for this.(Using Discord Webhooks)</p></div>
             <form className="w-full max-w-lg">
                 <div className="flex flex-wrap -mx-3 mb-6">
                     <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
